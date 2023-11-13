c21a548f8d8d88c52e361b98d2caef8a02eee99e
Fazliddin <99480480+FazliddinMirzaqosimov@users.noreply.github.com>
2023-11-13 10:10:06 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <BrowserRouter>
       <JWTAuthAuthProvider>
+        <img src="https://test-object.blr1.cdn.digitaloceanspaces.com/photo_2023-11-07_17-29-43.jpg" alt="lalala"/>
         <AppLayout />
       </JWTAuthAuthProvider>
     </BrowserRouter>
